15f89adc382d1e8d199542417917c65c3d810062
Barış Özgen <baris@megalodontrading.com>
2022-08-30 11:45:59 +0300

    NSubstite
    NSubstite dll added for creating fake player movement data

---
+++ TDDUnity/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TDDUnity.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Movement
+
+        #region Flip
+        #endregion
+        #region Jumps
+        #endregion
+        #region Attack
+        #endregion
+
+        #endregion
+
+        #region Healt
+        #endregion
+        #region Collect
+        #endregion
+        #region Animation
+        #endregion
+    }
+
+}
