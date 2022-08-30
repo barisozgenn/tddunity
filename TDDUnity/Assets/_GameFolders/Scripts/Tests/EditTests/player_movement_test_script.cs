4e1a6931e5322d26a59e78e6645eaaa299576d29
Barış Özgen <baris@megalodontrading.com>
2022-08-30 16:34:43 +0300

    refactor: mover unit test update

--- TDDUnity/Assets/_GameFolders/Scripts/Tests/EditTests/player_movement_test_script.cs
+++ TDDUnity/Assets/_GameFolders/Scripts/Tests/EditTests/player_movement_test_script.cs
@@ -11,11 +11,8 @@ namespace Movements
 {
     public class player_movement_test_script
     {
-        [Test]
-        public void move_10_meters_right_from_start_position()
+        private IPlayerController GetPlayer()
         {
-
-            #region Arrange: Get Reference what we will use
             IPlayerController playerController = Substitute.For<IPlayerController>();
 
             GameObject gObjExample = new GameObject();
@@ -23,13 +20,30 @@ namespace Movements
             playerController.transform.Returns(gObjExample.transform);
             playerController.InputReader = Substitute.For<IInputReader>();
 
-            IMover mover = new PlayerMoveWithTranslate(playerController: playerController);
+            return playerController;
+        }
+
+        private IMover GetMoverWithTranslate(IPlayerController playerController)
+        {
+           return new PlayerMoveWithTranslate(playerController: playerController);
+        }
+
+        [Test]
+        [TestCase(1f)]
+        [TestCase(-1f)]
+        public void move_10_meters_right_from_start_position(float horizontalInputValue)
+        {
+
+            #region Arrange: Get Reference what we will use
+            var playerController = GetPlayer();
+
+            IMover mover = GetMoverWithTranslate(playerController: playerController);
             Vector3 playerStartPosition = playerController.transform.position;
 
             #endregion
             #region Act: Use references we defined in Arrange
 
-            playerController.InputReader.Horizontal.Returns(1f);
+            playerController.InputReader.Horizontal.Returns(horizontalInputValue);
 
             for (int i = 0; i < 10; i++)
             {
@@ -55,14 +69,10 @@ namespace Movements
         {
 
             #region Arrange: Get Reference what we will use
-            IPlayerController playerController = Substitute.For<IPlayerController>();
 
-            GameObject gObjExample = new GameObject();
+            var playerController = GetPlayer();
 
-            playerController.transform.Returns(gObjExample.transform);
-            playerController.InputReader = Substitute.For<IInputReader>();
-
-            IMover mover = new PlayerMoveWithTranslate(playerController: playerController);
+            IMover mover = GetMoverWithTranslate(playerController: playerController);
             Vector3 playerStartPosition = playerController.transform.position;
 
             #endregion
@@ -89,44 +99,7 @@ namespace Movements
             #endregion
         }
 
-        [Test]
-        public void move_10_meters_left_from_start_position()
-        {
-
-            #region Arrange: Get Reference what we will use
-            IPlayerController playerController = Substitute.For<IPlayerController>();
-
-            GameObject gObjExample = new GameObject();
-
-            playerController.transform.Returns(gObjExample.transform);
-            playerController.InputReader = Substitute.For<IInputReader>();
-
-            IMover mover = new PlayerMoveWithTranslate(playerController: playerController);
-            Vector3 playerStartPosition = playerController.transform.position;
-
-            #endregion
-            #region Act: Use references we defined in Arrange
-
-            playerController.InputReader.Horizontal.Returns(-1f);
-
-            for (int i = 0; i < 10; i++)
-            {
-                mover.Tick();//get input
-                mover.FixedTick();//action with input
-            }
-
-            Vector3 playerEndPosition = playerController.transform.position;
-
-            Debug.Log("player start position => " + playerStartPosition);
-            Debug.Log("player end position => " + playerEndPosition);
-            #endregion
-
-            #region Assert: Get Result
-
-            Assert.Less(arg1: playerEndPosition.x, arg2: playerStartPosition.x);
-
-            #endregion
-        }
+       
     }
 }
 
